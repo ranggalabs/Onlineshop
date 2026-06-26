2ecb6b34c9ba7aae9b43ac5e4dd71e9dfae8dcc3
Rangga Prasetya <rangga@example.com>
2026-06-26 23:09:12 +0700

    feat: add Features, HomeCategory, and PopularProducts sections to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,16 @@
+import Features from "../components/Features"
 import Hero from "../components/Hero"
+import HomeCategory from "../components/HomeCategory"
+import PopularProducts from "../components/PopularProducts"
 
 
 const Home = () => {
     return (
         <div className="min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
             <Hero />
+            <Features />
+            <HomeCategory />
+            <PopularProducts />
         </div>
     )
 }
